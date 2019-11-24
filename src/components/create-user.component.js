a96d78f6d2be0e6275c4d0b34f9303c31646a023
sreenath <madhyastha@yahoo.com>
2019-11-24 00:13:24 -0500

    commited

---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CrateUsers extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Create Users </p>
+            </div>
+        )
+    }
+}
